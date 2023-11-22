07a2583f8a664fbf73de27d49aa5b19e49fb2429
Duyen <ngoc.ly135@gmail.com>
2023-11-23 01:17:47 +0700

    chatbot update

--- components/ChatApp/index.tsx
+++ components/ChatApp/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useLayoutEffect, useRef, useState } from 'react'
 import classNames from 'classnames/bind'
 import Style from "./ChatApp.module.scss"
 import { Image } from 'antd'
@@ -6,12 +6,22 @@ import { MinusOutlined, SendOutlined } from '@ant-design/icons'
 import "animate.css"
 import { useQuery } from 'react-query'
 import { getAnswer } from '../../api/ApiChat'
+import { useRouter } from 'next/router'
 const cx = classNames.bind(Style)
 export default function ChatApp() {
+    const router = useRouter()
     const [isOpen, setIsOpen] = useState(false);
-    const [question, setQuestion] = useState('');
-    const [chatList, setChatList] = useState([]);
-    const {data : answer} = useQuery(["answer", question], () => getAnswer(question))
+    const [question, setQuestion] = useState({
+        option_id: "",
+        question_name: ""
+    });
+    const [chatList, setChatList] = useState([{
+        question: "",
+        answer: "",
+        action: "",
+        image:""
+    }]);
+    const {data : answer} = useQuery(["answer", question.option_id], () => getAnswer(question.option_id))
     const list_answer = [
         {
             option_id: "chat_option_1",
@@ -43,13 +53,25 @@ export default function ChatApp() {
         }
     }
     const handleSelectQuestion = (quesion) => {
-        setChatList(prev => [...prev, quesion.question_name])
-        setQuestion(quesion.option_id)
-   
+        setQuestion(quesion)
     }
-
-    if(answer) console.log(answer)
-    console.log("chat", chatList)
+    useEffect(()=> {
+        if(answer) {
+            setChatList(prev => [...prev,{
+                question: question.question_name,
+                answer: answer.answer,
+                action: answer.action,
+                image: answer.image
+            }])
+        }
+    }, [answer])
+    useLayoutEffect(() => {
+        const chatListElement = document.getElementById("chatList");
+    
+        if (chatListElement) {
+          chatListElement.scrollTop = chatListElement.scrollHeight;
+        }
+      }, [chatList]);
   return (
     <div className={cx("chat-app")}>
         {!isOpen ?
@@ -64,7 +86,7 @@ export default function ChatApp() {
                         <MinusOutlined  className={cx("icon")}/>
                     </div>
                 </div>
-                <div className={cx("chat-list")}>
+                <div className={cx("chat-list")} id="chatList" style={{ scrollBehavior: 'smooth' }}>
                     <div className={cx("list-top")}>
                         
                             <Image src={require("../../assets/imgs/avt-logo.png").default.src} className={cx("list-logo")} preview={false} />
@@ -89,17 +111,66 @@ export default function ChatApp() {
                             </ul>
                         </div>
                     </div>
-                    { 
+                    { chatList.length > 1 ?
                         chatList.map((item, index) => {
+                            if(index !== 0)
                             return (
+                                <>
                                 <div className={cx("user")}>
-                                    <div className={cx("message")}>{item}</div>
+                                    <div className={cx("message")}>{item.question}</div>
                                 </div>
+                            
+                                    <div className={cx("bot")}>
+                                        <div className={cx("message")}>
+                                            {
+                                                item?.answer
+                                            }
+                                        </div>
+                                    </div>
+                                    {
+                                        item?.image !== null ?
+                                        <div className={cx("bot")}>
+                                            <div className={cx("message")}>Dưới đây là hướng dẫn của chúng tôi!
+                                                <Image src={item?.image}  className={cx("guide-img")} />
+                                            </div>
+                                        </div>
+                                        
+                                        : null
+                                    } 
+                                    {
+                                        item?.action !== null ?
+                                        <div className={cx("bot")}>
+                                            <div className={cx("message")}>Hãy tham quan cửa hàng của chúng tôi {" "}
+                                                <span 
+                                                    style={{fontWeight: '600', color: '#a58838', cursor: 'pointer'}}
+                                                    onClick={() => {router.push(`/${item.action}`)}}
+                                                >tại đây</span>  {" "}
+                                            nhé!</div>
+                                        </div>
+                                        
+                                        : null
+                                    }
+                                    <div className={cx("bot")}>
+                                        <div className={cx("message")}>Hãy chọn vấn đề bạn quan tâm nhé!
+                                            <ul className={cx('chat-options')}>
+                                                {
+                                                    list_answer.map((item) => {
+                                                        return (
+                                                            <li className={cx('chat-item')} onClick={() => handleSelectQuestion(item)}>
+                                                                {item.question_name}
+                                                            </li>
+                                                        )
+                                                    })
+                                                }
+                                            </ul>
+                                        </div>
+                                    </div>
+                                 </>
                             )
                         })
+                        :
+                        null
                     }
-                    
-                    
                 </div>
                 <div className={cx("chat-input")}>
                     <textarea rows={1}  className={cx("input")} placeholder="Bạn có thắc mắc gì..."/>
